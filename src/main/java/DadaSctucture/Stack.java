ea65aa73447792310845fc9339c7cfd43be07035
Joao Corsino <oi.corsino@gmail.com>
2024-06-15 12:51:06 -0300

    Estudo de pilhas

---
+++ src/main/java/DadaSctucture/Stack.java
@@ -0,0 +1,100 @@
+package DadaSctucture;
+
+public class Stack {
+
+    private Node top;
+    private int height;
+
+    public class Node
+    {
+        public int value;
+        Node next;
+
+        Node(int value)
+        {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public Stack(int value)
+    {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public void getTop()
+    {
+        if(top == null)
+        {
+            System.out.println("Pilha vazia");
+        }
+        System.out.println("Topo: " + top.value);
+    }
+
+    public void getHeight()
+    {
+        System.out.println("Altura: " + height);
+    }
+
+    public void print()
+    {
+        System.out.println("################");
+        Node temp = top;
+        while (temp != null)
+        {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+        System.out.println("################");
+    }
+
+    public void push(int value)
+    {
+        Node newNode = new Node(value);
+        if(height == 0)
+        {
+            top = newNode;
+        }
+        else
+        {
+           newNode.next = top;
+           top = newNode;
+        }
+        height++;
+    }
+
+    public Node pop()
+    {
+        if (height == 0) return null;
+
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+
+        return temp;
+    }
+
+    public static void inverter(final int[] numeros)
+    {
+        Stack stack = new Stack(numeros[0]);
+
+        for (int i = 1; i < numeros.length; i++)
+        {
+            stack.push(numeros[i]);
+        }
+
+        var node = stack.pop();
+        while (node != null)
+        {
+            System.out.println(node.getValue());
+            node = stack.pop();
+        }
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,16 @@
 package org.example;
 
-import DadaSctucture.LinkedList;
+import DadaSctucture.Stack;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        LinkedList list = new LinkedList("Elemento 1");
-        list.append("Elemento 2");
-        list.append("Elemento 3");
-        list.prepend("Elemento 0");
-
+        // Estudos LISTA <<
+        //LinkedList list = new LinkedList("Elemento 1");
+        //list.append("Elemento 2");
+        //list.append("Elemento 3");
+        //list.prepend("Elemento 0");
         //list.insert(3, "Elemento 2,5");
         //list.set(1, "Elemento 0,5");
         //System.out.println(list.get(2).data);
@@ -20,7 +20,19 @@ public class Main
         //list.getHead();
         //list.getTail();
         //list.getLength();
-        list.print();
+        //list.print();
 
+        // Estudos PILHA <<
+        //Stack myStack = new Stack(2);
+        //myStack.push(1);
+        //System.out.println("Removidos: " + myStack.pop().value);
+        //System.out.println("Removidos: " + myStack.pop().value);
+        //myStack.print();
+        //myStack.getTop();
+        //myStack.getHeight();
+
+        // inverter ordem da pilha <<
+        //int[] numeros = {5, 4, 3, 2, 1};
+        //Stack.inverter(numeros);
     }
 }
